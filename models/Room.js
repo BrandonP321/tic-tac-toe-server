15888b905f8b93520caeca995372348b4e3dd422
Brandon Phillips <brandon.phillips976@gmail.com>
2020-12-17 14:23:41 -0800

    added Room model

--- models/Room.js
+++ models/Room.js
@@ -3,7 +3,21 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-
+    passcode: {
+        type: String,
+        trim: true,
+        required: "Passcode required to create room"
+    },
+    player_one: {
+        type: String,
+        trim: true,
+        required: false
+    },
+    player_two: {
+        type: String,
+        trim: true,
+        required: false
+    }
 })
 
 const Room = mongoose.model("Room", schema)
